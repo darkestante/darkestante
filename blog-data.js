c337fdd5c3a5be4f027b53a00106e55a9351f088
darkestante <darkestante@gmail.com>
2026-04-25 23:14:21 -0300

    Publica artigo: CLIPE em Recife 2026: O Darkestante marcou presença no Circuito Literário de Pernambuco.

--- blog-data.js
+++ blog-data.js
@@ -25,7 +25,7 @@ export const BLOG_POSTS = [
         ]
       }
     ],
-    "updatedAt": "2026-04-26T02:10:24.779Z"
+    "updatedAt": "2026-04-26T02:14:19.835Z"
   },
   {
     "slug": "boas-vindas-ao-blog-darkestante",
